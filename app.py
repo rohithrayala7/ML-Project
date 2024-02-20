2434c21ac089918d080cdf8018397392ec7a4845
rohithrayala7 <rohithrayala7@gmail.com>
2024-02-20 13:04:10 -0600

    updated

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app=Flask(__name__)
 
+
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting machine learning project"
+    return "CI CD pipeline has been established."
 
 
 if __name__=="__main__":
